0d4f1da89c242b7a1b7f8f3f97ab5fb4f4874020
brian rienz erjas <erjasbrian@gmail.com>
2021-08-18 13:23:31 +0800

    fix bug

--- src/components/Header/HeaderHome.js
+++ src/components/Header/HeaderHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { NavLink } from 'react-router-dom';
 import style from '../../style/HeaderHome.module.css';
 import DateTimeCounter from './DateTimeCounter';
@@ -6,12 +6,11 @@ import { HiMenu } from "react-icons/hi";
 import ChangeTheme from './ChangeTheme';
 import ChangeLang from './ChangeLang';
 import Login from './Login';
-import { useContext } from 'react/cjs/react.development';
 import { Context } from '../../context/Context';
 
 function Header() {
 
-    const { theme, changeTheme, flag, changeLang, auth, userInfo} = useContext(Context);
+    const { theme, changeTheme, flag, changeLang, auth, userInfo } = useContext(Context);
     
     const [toggle, setToggle] = useState(false);
 
@@ -25,7 +24,7 @@ function Header() {
 
                     <label
                         htmlFor='navbar'
-                        onClick={() => setToggle(true)}
+                        onClick={() => setToggle(!toggle)}
                         className={style.toggleBtn}
                     >
                         <HiMenu className={style.toggleBtnIcon}/>
