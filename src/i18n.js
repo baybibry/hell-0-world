6526a6566dd2e3d881da679c2ac96e462104d0f1
brian rienz erjas <erjasbrian@gmail.com>
2021-08-18 12:10:59 +0800

    new file

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.on('changeLang', function(lang){
 
@@ -46,7 +45,7 @@ const resources = {
 }
 
 
-i18n.use(initReactI18next).use(LanguageDetector)
+i18n.use(initReactI18next)
 .init(
     {
         resources,
