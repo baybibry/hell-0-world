8dcfa82f5b83e2d8e52a215dffd701aa44d0e18b
brian rienz erjas <erjasbrian@gmail.com>
2021-08-18 12:07:19 +0800

    update page layout and content

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Context } from '../context/Context'
-import style from '../style/Home.module.css'
-import { useTranslation } from 'react-i18next'
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../context/Context';
+import style from '../style/Home.module.css';
+import { useTranslation } from 'react-i18next';
 import IconContainer from '../components/IconsContainer/IconContainer';
 import fontIcons from '../iconsData';
 import Notification from '../components/Notification/Notification';
@@ -13,7 +13,7 @@ function Home() {
     const [search, setsearch] = useState('')
     const [icons, seticons] =  useState()
 
-    const {t, i18n} = useTranslation();
+    const { t } = useTranslation();
 
     useEffect(() => {
 
@@ -35,59 +35,67 @@ function Home() {
         <div
             className={`${theme ? 'text-dark dark' : 'text-light light'} ${style.body}`}
         >
-            <div className={style.notificationCon}>
-                {
-                    notifs.map((obj, i) => (
-                        <Notification 
-                            key={i + obj.text}
-                            text={obj.text}
-                            id={obj.id}
-                            removed={removeNotif}
-                        />
-                    ))
-                }
-            </div>
 
-            <h1 className={style.title}>
-                Aios Font Icons
-            </h1>
 
-            <div className={style.search}>
+            {/* <HeaderHome/> */}
 
-                <input 
-                    type='text' 
-                    onChange={ e => setsearch(e.target.value)}
-                    placeholder={t('search')}
+            <div className={style.bodyMaxWidth}>
+
+                <div className={style.notificationCon}>
+                    {
+                        notifs.map((obj, i) => (
+                            <Notification 
+                                key={i + obj.text}
+                                text={obj.text}
+                                id={obj.id}
+                                removed={removeNotif}
+                            />
+                        ))
+                    }
+                </div>
+
+                <h1 className={style.title}>
+                    Aios Font Icons
+                </h1>
+
+                <div className={style.search}>
+
+                    <input 
+                        type='text' 
+                        onChange={ e => setsearch(e.target.value)}
+                        placeholder={t('search')}
+                    />
+                    
+                    <i className="ai-font-magnifying-glass-a"></i>
+
+                </div>
+
+                <IconContainer 
+                    data={icons}
+                    copied={addNotif}
                 />
-                
-                <i className="ai-font-magnifying-glass-a"></i>
-
-            </div>
-
-            <IconContainer 
-                data={icons}
-                copied={addNotif}
-            />
 
 
-            <footer className={style.footer}>
-                
-                <hr className={style.hr}/>
+                <footer className={style.footer}>
+                    
+                    <hr className={style.hr}/>
 
-                <h3 className={style.footerLink}>
+                    <h3 className={style.footerLink}>
 
-                    Font Source <a 
-                        
-                        target='__blank' 
-                        className={`${theme ? 'text-dark' : 'text-light'}`}
-                        href='https://resources.agentimage.com/fonts/agentimage.font.icons.css?ver=5.3.4'
-                        
-                        > https://resources.agentimage.com/fonts/agentimage.font.icons.css?ver=5.3.4 
-                        
-                        </a>
-                
-                </h3>
-            </footer>
+                        Font Source <a 
+                            
+                            target='__blank' 
+                            className={`${theme ? 'text-dark' : 'text-light'}`}
+                            href='https://resources.agentimage.com/fonts/agentimage.font.icons.css?ver=5.3.4'
+                            
+                            > https://resources.agentimage.com/fonts/agentimage.font.icons.css?ver=5.3.4 
+                            
+                            </a>
+                    
+                    </h3>
+                </footer>
+
+            </div> 
 
         </div>
     )
--- src/page/News.js
+++ src/page/News.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import NewsCon from '../components/NewsContainer/NewsCon'
+import { Context } from '../context/Context';
 
 function News() {
+
+    const { theme, news } = useContext(Context);
+    
     return (
-        <div>
-            news
+        <div
+            className={`${theme ? 'text-dark dark' : 'text-light light'}`}
+        >
+
+
+            <NewsCon news={news}/>
+
         </div>
     )
 }
